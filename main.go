01515fa05316da898e125b03ed776de524edb772
Norbert Hambuer <hambuernorbi@yahoo.com>
2019-11-07 22:14:06 +0200

    Corectii

--- main.go
+++ main.go
@@ -57,8 +57,43 @@ func citireMatriceIncidenta() [][]int {
 	return matrix
 }
 
-func citireListaArce() ([]int, []int) {
+func citireListaArce() (int, []int, []int) {
 	file, err := ioutil.ReadFile("listaArce.txt")
+
+	if err != nil {
+		fmt.Println("Eroare citire matrice de incidenta.")
+	}
+
+	e1, e2 := []int{}, []int{}
+
+	lines := strings.Split(string(file), "\r")
+	var val, n int
+
+	for i, line := range lines {
+		vals := strings.Split(strings.TrimPrefix(line, "\n"), " ")
+
+		if len(vals) == 1 {
+			val, _ = strconv.Atoi(vals[0])
+			n = val
+			continue
+		}
+
+		for _, value := range vals {
+
+			val, _ = strconv.Atoi(value)
+			if i == 1 {
+				e1 = append(e1, val)
+			} else {
+				e2 = append(e2, val)
+			}
+		}
+	}
+
+	return n, e1, e2
+}
+
+func citireListaSuc() ([]int, []int) {
+	file, err := ioutil.ReadFile("listaSuccesori.txt")
 
 	if err != nil {
 		fmt.Println("Eroare citire matrice de incidenta.")
@@ -86,8 +121,8 @@ func citireListaArce() ([]int, []int) {
 	return e1, e2
 }
 
-func citireListaSuc() ([]int, []int) {
-	file, err := ioutil.ReadFile("listaSuccesori.txt")
+func citireListaPre() ([]int, []int) {
+	file, err := ioutil.ReadFile("listaPredecesori.txt")
 
 	if err != nil {
 		fmt.Println("Eroare citire matrice de incidenta.")
@@ -356,7 +391,7 @@ func getNrNoduri(listaArce []int) int {
 	}
 
 	nr := 1
-	lastNode := -1
+	lastNode := 1
 	for _, val := range listaArce {
 		if lastNode != val {
 			lastNode = val
@@ -367,8 +402,8 @@ func getNrNoduri(listaArce []int) int {
 	return nr
 }
 
-func listaArceToAdiacenta(v []int, w []int) [][]int {
-	n := getNrNoduri(v)
+func listaArceToAdiacenta(n int, v []int, w []int) [][]int {
+	//n := getNrNoduri(v)
 
 	matrix := make([][]int, n)
 	for i := 0; i < n; i++ {
@@ -382,8 +417,8 @@ func listaArceToAdiacenta(v []int, w []int) [][]int {
 	return matrix
 }
 
-func listaArceToIncidenta(v []int, w []int) [][]int {
-	n := getNrNoduri(v)
+func listaArceToIncidenta(n int, v []int, w []int) [][]int {
+	//n := getNrNoduri(v)
 	m := len(v)
 
 	matrix := make([][]int, n)
@@ -406,9 +441,9 @@ func listaArceToIncidenta(v []int, w []int) [][]int {
 	return matrix
 }
 
-func listaArceToListaSuc(v []int, w []int) ([]int, []int) {
+func listaArceToListaSuc(n int, v []int, w []int) ([]int, []int) {
 	e1 := []int{}
-	n := getNrNoduri(v)
+	//n := getNrNoduri(v)
 	var found bool
 
 	poz := 1
@@ -433,10 +468,10 @@ func listaArceToListaSuc(v []int, w []int) ([]int, []int) {
 	return e1, w
 }
 
-func listaArceToListaPre(v []int, w []int) ([]int, []int) {
+func listaArceToListaPre(n int, v []int, w []int) ([]int, []int) {
 	e1 := []int{}
 	e2 := []int{}
-	n := getNrNoduri(v)
+	//n := getNrNoduri(v)
 	var found bool
 
 	poz := 1
@@ -462,62 +497,340 @@ func listaArceToListaPre(v []int, w []int) ([]int, []int) {
 	return e1, e2
 }
 
+func listaSucToAdiacenta(v []int, w []int) [][]int {
+	n := len(v) - 1
+	m := len(w) - 1
+
+	matrix := make([][]int, n)
+	for i := 0; i < n; i++ {
+		matrix[i] = make([]int, n)
+	}
+
+	for i := 0; i < n; i++ {
+		for j := v[i]; j < v[i+1]; j++ {
+			matrix[i][w[j-1]-1] = 1
+		}
+	}
+
+	if v[n] != v[n-1] {
+		matrix[n-1][w[m]-1] = 1
+	}
+
+	return matrix
+}
+
+func listaSucToIncidenta(v []int, w []int) [][]int {
+	n := len(v) - 1
+	m := len(w)
+
+	matrix := make([][]int, n)
+	for i := 0; i < n; i++ {
+		matrix[i] = make([]int, m)
+	}
+
+	poz := 0
+	for i := 0; i < n; i++ {
+		for j := v[i]; j < v[i+1]; j++ {
+			matrix[i][poz] = 1
+			matrix[w[j-1]-1][poz] = -1
+			poz++
+		}
+	}
+
+	if v[n] != v[n-1] {
+		matrix[w[m-1]-1][poz] = -1
+		matrix[n-1][poz] = 1
+	}
+
+	return matrix
+}
+
+func listaSucToListaArce(v []int, w []int) ([]int, []int) {
+	e1 := []int{}
+	e2 := []int{}
+
+	n := len(v) - 1
+
+	for i := 0; i < n; i++ {
+		for j := v[i]; j < v[i+1]; j++ {
+			e1 = append(e1, i+1)
+			e2 = append(e2, w[j-1])
+		}
+	}
+
+	if v[n] != v[n-1] {
+		m := len(w)
+
+		e1 = append(e1, n)
+		e2 = append(e2, w[m-1])
+	}
+
+	return e1, e2
+}
+
+func listaSucToListaPre(v []int, w []int) ([]int, []int) {
+	e1 := []int{}
+	e2 := []int{}
+	n := len(v) - 1
+	var found bool
+	var node, nodeIndex int
+
+	poz := 1
+
+	for i := 0; i < n; i++ {
+		e1 = append(e1, poz)
+		found = false
+
+		for j := 0; j < len(w); j++ {
+			if w[j]-1 == i {
+				found = true
+
+				node = 0
+				nodeIndex = v[0]
+
+				for nodeIndex-1 <= j && node < n {
+					node++
+					nodeIndex = v[node]
+				}
+
+				e2 = append(e2, node)
+				poz++
+			}
+		}
+
+		if !found {
+			poz++
+		}
+	}
+
+	e1 = append(e1, poz-1)
+
+	return e1, e2
+}
+
+func listaPreToAdiacenta(v []int, w []int) [][]int {
+	n := len(v) - 1
+	m := len(w)
+
+	matrix := make([][]int, n)
+	for i := 0; i < n; i++ {
+		matrix[i] = make([]int, n)
+	}
+
+	for i := 0; i < n; i++ {
+
+		for j := v[i]; j < v[i+1]; j++ {
+			matrix[w[j-1]-1][i] = 1
+		}
+	}
+
+	if v[n] != v[n-1] {
+		matrix[w[m-1]-1][n-1] = 1
+	}
+
+	return matrix
+}
+
+func listaPreToIncidenta(v []int, w []int) [][]int {
+	n := len(v) - 1
+	m := len(w)
+
+	matrix := make([][]int, n)
+	for i := 0; i < n; i++ {
+		matrix[i] = make([]int, m)
+	}
+
+	var node, nodeIndex int
+	poz := 0
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if w[j]-1 == i {
+				matrix[i][poz] = 1
+
+				node = 0
+				nodeIndex = v[0]
+
+				for nodeIndex-1 <= j && node < n {
+					node++
+					nodeIndex = v[node]
+				}
+
+				matrix[node-1][poz] = -1
+
+				poz++
+			}
+		}
+	}
+
+	return matrix
+}
+
+func listaPreToListaArce(v []int, w []int) ([]int, []int) {
+	e1 := []int{}
+	e2 := []int{}
+
+	n := len(v) - 1
+	m := len(w)
+
+	var node, nodeIndex int
+	poz := 0
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if w[j]-1 == i {
+
+				e1 = append(e1, i+1)
+
+				node = 0
+				nodeIndex = v[0]
+
+				for nodeIndex-1 <= j && node < n {
+					node++
+					nodeIndex = v[node]
+				}
+
+				e2 = append(e2, node)
+
+				poz++
+			}
+		}
+	}
+
+	return e1, e2
+}
+
+func listaPreToListaSuc(v []int, w []int) ([]int, []int) {
+	e1 := []int{}
+	e2 := []int{}
+
+	n := len(v) - 1
+	m := len(w)
+
+	var node, nodeIndex int
+	var found bool
+	poz := 1
+
+	for i := 0; i < n; i++ {
+
+		e1 = append(e1, poz)
+		found = false
+		for j := 0; j < m; j++ {
+			if w[j]-1 == i {
+				found = true
+
+				node = 0
+				nodeIndex = v[0]
+
+				for nodeIndex-1 <= j && node < n {
+					node++
+					nodeIndex = v[node]
+				}
+
+				e2 = append(e2, node)
+
+				poz++
+			}
+		}
+
+		if !found {
+			poz++
+		}
+	}
+
+	e1 = append(e1, poz-1)
+
+	return e1, e2
+}
+
 func main() {
-	/*	matrix := citireMatriceAdiacenta()
-		fmt.Println(matrix)
+	/*		matrix := citireMatriceAdiacenta()
+			fmt.Println(matrix)
 
-		matrixI := adiacentaToIncidenta(matrix)
-		fmt.Println(matrixI)
+			matrixI := adiacentaToIncidenta(matrix)
+			fmt.Println(matrixI)
 
-		e1,e2 := adiacentaToListaArce(matrix)
+			e1,e2 := adiacentaToListaArce(matrix)
+			fmt.Println(e1)
+			fmt.Println(e2)
+
+			s1, s2 := adiacentaToListaSuc(matrix)
+			fmt.Println(s1)
+			fmt.Println(s2)
+
+			p1, p2 := adiacentaToListaPre(matrix)
+			fmt.Println(p1)
+			fmt.Println(p2)*/
+
+	/*		matrix := citireMatriceIncidenta()
+			fmt.Println(matrix)
+
+			matrixI := incidentaToAdiacenta(matrix)
+			fmt.Println(matrixI)
+
+			e1, e2 := incidentaToListaArce(matrix)
+			fmt.Println(e1)
+			fmt.Println(e2)
+
+			s1, s2 := incidentaToListaSuc(matrix)
+			fmt.Println(s1)
+			fmt.Println(s2)
+
+			p1, p2 := incidentaToListaPre(matrix)
+			fmt.Println(p1)
+			fmt.Println(p2)*/
+
+	/*	n, e1, e2 := citireListaArce()
+		fmt.Println(n)
 		fmt.Println(e1)
 		fmt.Println(e2)
 
-		s1, s2 := adiacentaToListaSuc(matrix)
+		matrix := listaArceToAdiacenta(n, e1, e2)
+		fmt.Println(matrix)
+
+		matrixI := listaArceToIncidenta(n, e1, e2)
+		fmt.Println(matrixI)
+
+		s1, s2 := listaArceToListaSuc(n, e1, e2)
 		fmt.Println(s1)
 		fmt.Println(s2)
 
-		p1, p2 := adiacentaToListaPre(matrix)
+		p1, p2 := listaArceToListaPre(n, e1, e2)
 		fmt.Println(p1)
 		fmt.Println(p2)*/
 
-	/*	matrix := citireMatriceIncidenta()
-		fmt.Println(matrix)
-
-		matrixI := incidentaToAdiacenta(matrix)
-		fmt.Println(matrixI)
-
-		e1, e2 := incidentaToListaArce(matrix)
+	/*	e1, e2 := citireListaSuc()
 		fmt.Println(e1)
 		fmt.Println(e2)
 
-		s1, s2 := incidentaToListaSuc(matrix)
-		fmt.Println(s1)
-		fmt.Println(s2)
-
-		p1, p2 := incidentaToListaPre(matrix)
-		fmt.Println(p1)
-		fmt.Println(p2)*/
-
-	/*	e1, e2 := citireListaArce()
-		fmt.Println(e1)
-		fmt.Println(e2)
-
-		matrix := listaArceToAdiacenta(e1, e2)
+		matrix := listaSucToAdiacenta(e1, e2)
 		fmt.Println(matrix)
 
-		matrixI := listaArceToIncidenta(e1, e2)
+		matrixI := listaSucToIncidenta(e1, e2)
 		fmt.Println(matrixI)
 
-		s1, s2 := listaArceToListaSuc(e1, e2)
+		s1, s2 := listaSucToListaArce(e1, e2)
 		fmt.Println(s1)
 		fmt.Println(s2)
 
-		p1, p2 := listaArceToListaPre(e1, e2)
+		p1, p2 := listaSucToListaPre(e1, e2)
 		fmt.Println(p1)
 		fmt.Println(p2)*/
 
-	e1, e2 := citireListaSuc()
-	fmt.Println(e1)
-	fmt.Println(e2)
+	/*		e1, e2 := citireListaPre()
+			fmt.Println(e1)
+			fmt.Println(e2)
+
+			matrix := listaPreToAdiacenta(e1, e2)
+			fmt.Println(matrix)
+
+			matrixI := listaPreToIncidenta(e1, e2)
+			fmt.Println(matrixI)
+
+			s1, s2 := listaPreToListaArce(e1, e2)
+			fmt.Println(s1)
+			fmt.Println(s2)
+
+			p1, p2 := listaPreToListaSuc(e1, e2)
+			fmt.Println(p1)
+			fmt.Println(p2)*/
+
 }
